37737f02f1791886f0215d356e38859a87506c78
SK Digital <sakhilemaxwell@gmail.com>
2026-04-02 11:38:31 +0000

    Fix: Correct newline characters in input-error.blade.php

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,1 +1,7 @@
-@props(['for'])\n\n@error($for)\n    <p {{ $attributes->merge(['style' => 'margin-top: 6px; font-size: 12px; color: var(--red);']) }}>\n        {{ $message }}\n    </p>\n@enderror
+@props(['for'])
+
+@error($for)
+    <p {{ $attributes->merge(['style' => 'margin-top: 6px; font-size: 12px; color: var(--red);']) }}>
+        {{ $message }}
+    </p>
+@enderror
